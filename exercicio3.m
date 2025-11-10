a919dffbca422744b82f72f40ea763b22e5b52e8
limapereiralucas <limapereiralucas@gmail.com>
2025-11-10 13:59:44 -0300

    Lucas Miguel exercicio3.m

--- exercicio3.m
+++ exercicio3.m
@@ -5,11 +5,23 @@ function [mu] = exercicio3(t, NC)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
+% Corrigindo o vetor de tempo para começar em zero
+t = t - t(1);
 
+% Calculando o logaritmo natural do número de células
+lnNC = log(NC); 
+lnN0 = p(2);  %Intercepto da reta (ln do número inicial de células)
+
+
+
+% Ajustar uma reta ln(NC) = ln(N0) + μ*t
+p = polyfit(t, lnNC, 1);
+
+%ln(NC)≈p(1)t+p(2)
+
+mu = p(1);
 
 % mantenha essas duas linhas finais, subtituindo mu pelo valor que você calculou
-mu = 0;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
