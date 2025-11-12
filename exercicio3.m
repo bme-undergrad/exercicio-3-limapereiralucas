5ce3180d87ffd078da8973b7bba2d82be9b0ff4b
limapereiralucas <limapereiralucas@gmail.com>
2025-11-12 09:34:35 -0300

    Update exercicio3.m

--- exercicio3.m
+++ exercicio3.m
@@ -5,21 +5,27 @@ function [mu] = exercicio3(t, NC)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Corrigindo o vetor de tempo para começar em zero
-t = t - t(1);
+% Garante que t comece em zero
+  t = t - t(1);
 
-% Calculando o logaritmo natural do número de células
-lnNC = log(NC); 
-lnN0 = p(2);  %Intercepto da reta (ln do número inicial de células)
+  % Transforma os dados em log natural
+  Y = log(NC);
 
+  % Ajuste linear das primeiras duas horas
+  idx1 = find(t <= 2);
+  p1 = polyfit(t(idx1), Y(idx1), 1);
+  mu1 = p1(1);
 
+  % Ajuste linear das ultimas quatro horas 
+  idx2 = find(t >= 2.5);
+  p2 = polyfit(t(idx2), Y(idx2), 1);
+  mu2 = p2(1);
 
-% Ajustar uma reta ln(NC) = ln(N0) + μ*t
-p = polyfit(t, lnNC, 1);
+  % Exibindo os resultados 
+  printf("Taxa de crescimento nas primeiras 2h: %.4f 1/h\n", mu1);
+  printf("Taxa de crescimento nas próximas 4h: %.4f 1/h\n", mu2);
+  mu = [mu1; mu2]
 
-%ln(NC)≈p(1)t+p(2)
-
-mu = p(1);
 
 % mantenha essas duas linhas finais, subtituindo mu pelo valor que você calculou
 
